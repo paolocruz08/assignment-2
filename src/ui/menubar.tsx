cb0e775a6cd1549f7a2a71333ac8a9b4013603e5
Huddwin <huddwin.almeida@edu.sait.ca>
2026-02-04 15:40:26 -0700

    Update menubar.tsx

--- src/ui/menubar.tsx
+++ src/ui/menubar.tsx
@@ -1,4 +1,7 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
 import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 import { StyleSheet, View } from "react-native";
 
 export default function MenuBar() {
@@ -6,7 +9,10 @@ export default function MenuBar() {
     <View style={styles.menuBar}>
       <Ionicons name="home-outline" size={24} color="black" />
       <Ionicons name="search" size={24} color="black" />
-    </View>
+      <AntDesign name="plus" size={24} color="black" />
+      <Ionicons name="bag" size={24} color="black" />
+      <MaterialCommunityIcons name="account-circle" size={24} color="black" />
+</View>
   );
 }
 
