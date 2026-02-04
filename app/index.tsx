4bd09c473bf66558d7e96698867d83cbca12cc18
Paolo Cruz <paolo.cruz24@gmail.com>
2026-02-03 18:48:29 -0700

    Added Avatar section

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,20 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Header from '../src/ui/header';
+import Avatar from "../src/ui/avatar";
+import Header from "../src/ui/header";
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
-      
+      <Avatar />
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
